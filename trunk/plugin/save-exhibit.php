e179ca84141b9e826be1c12f1feec42bf0be5e35
edward.benson <edward.benson@6d90720a-02ad-11de-994d-8308d8631604>
2009-03-05 22:29:28 +0000

    save it

---
+++ trunk/plugin/save-exhibit.php
@@ -0,0 +1,12 @@
+<?
+/* This file saves, or updates, an exhibit to the database (depending on whether an ID is present).
+ * it returns (JSONP-style) a callback with the Exhibit ID in the database. 
+ *
+ *
+ * TODO:
+ *  - Protect so that only admins can load this page
+ *  - Write the code to save it
+ */
+
+
+?>
