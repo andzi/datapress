f84c728e7cae2eb0eb91aed5ef4e256ad955a7a8
marcua <marcua@6d90720a-02ad-11de-994d-8308d8631604>
2009-03-05 21:30:48 +0000

    first stab at exhibit configuration lightbox

--- plugin/exhibit_admin_include.php
+++ plugin/exhibit_admin_include.php
@@ -4,6 +4,19 @@ if (!$guessurl = site_url())
 $baseuri = $guessurl;
 $exhibituri = $baseuri . '/wp-content/plugins/datapress';
 ?>
+
+    	<script src="<?php echo $exhibituri ?>/js/jquery-1.2.6.min.js" type="text/javascript"></script>
+        <script src="<?php echo $exhibituri ?>/js/jquery.fancybox-1.0.0.js" type="text/javascript"></script> 
+        <link rel="stylesheet" href="<?php echo $exhibituri ?>/css/fancy.css" type="text/css" media="screen"/>
+    
+  		<script type="text/javascript">
+        $(document).ready(function() { 
+        	$('a.exhibit_link').fancybox({ 'frameWidth': $('body').width(), 'frameHeight': $('body').height() });
+		});
+        </script>
+
+
+
 <script type='text/javascript' src='<?php echo $baseuri ?>/wp-includes/js/jquery/ui.tabs.js?ver=1.5.1'></script>
 <script type='text/javascript' src='<?php echo $exhibituri ?>/sheet/jquery.sheet.calc.js'></script>
 <script type='text/javascript' src='<?php echo $exhibituri ?>/sheet/jquery.sheet.js'></script>
