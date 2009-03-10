8abc2179e8495118b2dc86e6715b62f8fc42f979
edward.benson <edward.benson@6d90720a-02ad-11de-994d-8308d8631604>
2009-03-10 18:21:25 +0000

    ted updates

--- plugin/configurator/exhibit-inputbox-view-bar.php
+++ plugin/configurator/exhibit-inputbox-view-bar.php
@@ -1,15 +1,15 @@
 <p><b>A <b>Bar Chart</b> displays numeric data as set of bars.</b></p>
 
 <form id="add-view-bar-form">
-	<p><i>Visualization Title</i><br /><input id="view-bar-label" type="text" size="30" /></p>
 
 	<table>
-		<tr><td><i>X Axis Value</i></td><td>X Axis Label</td><td></td></tr>
-		<tr><td><select id="view-bar-x" class="allpropbox"></select></td><td><input id="view-bar-xLabel" /></td><td></td></tr>
-		<tr><td><i>Y Axis Value</i></td><td>Y Axis Label</td><td></td></tr>
-		<tr><td><select id="view-bar-y" class="allpropbox"></select></td><td></td></tr>
+		<tr><td><i>Visualization Title</i></td><td><input id="view-bar-label" type="text" size="30" /></td><td></td></tr>
+		<tr><td><i>X Axis</i></td><td><select id="view-bar-x" class="allpropbox"></select></td><td></td></tr>
+		<tr><td><i>X Axis Label</i></td><td><input id="view-bar-xLabel" /></td><td></td></tr>
+		<tr><td><i>Y Axis</i></td><td><select id="view-bar-y" class="allpropbox"></select></td><td></td></tr>
+		<tr><td><i>Y Axis Label</i></td><td><input id="view-bar-yLabel" /></td><td></td></tr>
 		<tr><td><i>Scale</i></td><td><select id="view-bar-xScale"><option value="linear">Linear</option><option value="log">Log</option></select></td><td></td></tr>
-		<tr><td><i>Extra Attributes</i>/td><td><input id="view-bar-extra-attributes" type="text" /></td><td>(optional)</td></tr>
+		<tr><td><i>Extra Attributes</i></td><td><input id="view-bar-extra-attributes" type="text" /></td><td>(optional)</td></tr>
 	</table>
 	<br />
 	<p align="right"><a href="#" class="addlink" onclick="submit_view_bar_facet(); return false">Add Bar Chart</a></p>
