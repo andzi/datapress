37440b7197a28c537c3b82d713a5cadf33a2147c
marcua <marcua@6d90720a-02ad-11de-994d-8308d8631604>
2009-03-13 20:19:39 +0000

    removing unused data input mechanisms

--- trunk/plugin/configurator/exhibit-inputbox-data-upload.php
+++
@@ -1,5 +0,0 @@
-<p><b>Import data from a file on your computer or another source on the web. This will create a duplicate copy of the data that you host on your blog.</b></p>
-
-	<p>Upload a File:<input type="file" id="upload-doc-id" type="text" size="30" /></p>
-    <p>Dataset Name: <input id="data-upload-sourcename" type="text" size="30" /></p>
-	<p><a href="#" class="addlink" onclick="return false;">Upload</a></p>
