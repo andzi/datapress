ba76469c63e4d79ec1927e2302381f9a1b6f73fd
marcua <marcua@6d90720a-02ad-11de-994d-8308d8631604>
2009-03-18 18:54:26 +0000

    added editing for lenses

--- plugin/configurator/exhibit-inputbox-lenses.php
+++ plugin/configurator/exhibit-inputbox-lenses.php
@@ -15,50 +15,15 @@
 </div>
 </div>
 
-<div class="inner-tabs-panel">
-	<table>
-		<tr>
-			<td>Create a New Lens for</td>
-			<td><select id="lensfor" class="alltypebox"></select></td>
-		</tr>
-	</table>
-<div style="height: 150px;" id='lenscontainer'>
-	<textarea id="lens-text" class='' style="height: 150px; width: 80%;" name='lens-text'></textarea>
+<div id="exhibit-lenses-tabs">
+	<ul id="ex-lenses-tabs-list" class="inner-tabs">
+		<li class="ui-tabs-selected"><a href="#exhibit-lenses-edit" >List</a></li>
+    </ul>
+	<div id="exhibit-lenses-edit" class="inner-tabs-panel"><?php include('exhibit-inputbox-lense-edit.php'); ?></div>
 </div>
-
-<table width="100%">
-	<tr>
-		<td><p align="left">Available Properties: <select id="lense-prop-possibilities" class="allpropbox"></select><a href="#" onClick="appendToLens('{{.' + jQuery('#lense-prop-possibilities').val() + '}}'); return false;">Insert Property</a></p></td>
-		<td><p align="right"><a href="#" class="addlink" onclick="submit_lens(); return false">Add Lens</a></p></td>
-	</tr>
-</table>
-
-</div>
-
-<script>
-	$(document).ready(function(){
-		// <![CDATA[
-		// var win = window.dialogArguments || opener || parent || top;
-		// var lens_editor = window.tinyMCE.execCommand('mceAddControl', false, 'lens-text');
-		// ]]>
-	});
-</script>
 
 <script type="text/javascript">
-function submit_lens() {
-		// var win = window.dialogArguments || opener || parent || top;
-        var klass = jQuery('#lensfor').val();
-        var html = jQuery('#lens-text').val(); // win.tinyMCE.getInstanceById('lens-text').getContent(); // 
-        var kind = 'lens';
-        
-        addExhibitElementLink(
-                "lens-list", 
-                "Lens for " + klass, 
-                'lens',
-                {
-                        kind: kind,
-                        'class': klass,
-                        html: html 
-        });        
-}
+jQuery(document).ready(function(){
+	var datasource_tabs = jQuery("#exhibit-lenses-tabs > ul").tabs();
+});
 </script>
