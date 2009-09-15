3f7321d34b897b6b841eb87121265db23a663726
edward.benson <edward.benson@6d90720a-02ad-11de-994d-8308d8631604>
2009-09-15 06:05:29 +0000

    Adding more attractive stylesheet

--- trunk/plugin/head-exhibit.php
+++ trunk/plugin/head-exhibit.php
@@ -4,10 +4,14 @@ global $exhibits_to_show;
 if (count($exhibits_to_show) == 1) {
     if ((! $exhibits_to_show[0]->get('lightbox')) || (isset($lightboxed_exhibit))) {
         $google_map_api_key = get_option( 'google_map_api_key' );
+        $plugin_dir = trailingslashit( get_bloginfo('wpurl') ).PLUGINDIR.'/datapress';    
+
         ?>
         <script src="http://api.simile-widgets.org/exhibit/2.2.0/exhibit-api.js?autoCreate=false" type="text/javascript"></script> 
         <script src="http://api.simile-widgets.org/exhibit/2.2.0/extensions/time/time-extension.js" type="text/javascript"></script>
         <script src="http://api.simile-widgets.org/exhibit/2.2.0/extensions/chart/chart-extension.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo $plugin_dir ?>/exhibit.css" />
+                
         <?php 
         if ($google_map_api_key != null) { 
             ?><script src="http://api.simile-widgets.org/exhibit/2.2.0/extensions/map/map-extension.js?gmapkey=<?php echo $google_map_api_key ?>"></script><?php
