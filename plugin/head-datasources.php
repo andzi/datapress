be5d8ff20f17db3589aeb316f32e2bba05f68b79
edward.benson <edward.benson@6d90720a-02ad-11de-994d-8308d8631604>
2011-05-11 20:18:41 +0000

    stuff

--- plugin/head-datasources.php
+++ plugin/head-datasources.php
@@ -12,9 +12,9 @@ if (isset($exhibits_to_show) && (count($exhibits_to_show) > 0)) {
             echo($datasource->htmlContent() . "\n");
         }
 
-	if(WpExhibitGeocoder::doesExhibitContainGeocodedData($exhibit_to_show->get('id'))) {
-        echo("<link href='$exhibituri/wp-exhibit-geocode.php?exhibit-id=" . $exhibit_to_show->get('id') . "' type='application/json' rel='exhibit/data' alt='geocoded_data' />\n");
-	}
+    // if(WpExhibitGeocoder::doesExhibitContainGeocodedData($exhibit_to_show->get('id'))) {
+    //         echo("<link href='$exhibituri/wp-exhibit-geocode.php?exhibit-id=" . $exhibit_to_show->get('id') . "' type='application/json' rel='exhibit/data' alt='geocoded_data' />\n");
+    // }
 }
 }
 ?>
